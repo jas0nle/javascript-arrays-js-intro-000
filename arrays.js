57a05c64e7a19f29a7f9a3c7a4805dbac2ae3e98
Jason Le <jas0nle@yahoo.com>
2019-05-29 18:46:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -50,6 +50,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 
 function removeElementFromEndOfArray(array) {
   var Array = array;
-  Array.pop();
+  Array.slice(0, Array.length - 1);
   return Array;
 }
